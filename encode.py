39626614a8acd0227bc24beb624c4f088e0417b1
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-12-31 09:05:03 +0900

    add argument error check.

--- encode.py
+++ encode.py
@@ -1,6 +1,13 @@
 # [DOCS]
 # https://docs.python.jp/3/library/codecs.html
 import codecs
+import sys
 
-enc = "Pnrfne SYNT{PGS_Rnfl}"
+argv = sys.argv
+
+if (len(argv) != 2):
+    print 'Argument is less. Please add char'
+    exit()
+
+enc = argv[1]
 print codecs.decode(enc, 'rot13') # rot13 is seaser encryption.
