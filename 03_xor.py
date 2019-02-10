17325599789ce280eab90d4f5d1c5c6f3792c9f8
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-02-10 19:55:21 +0900

    edit

--- 03_xor.py
+++ 03_xor.py
@@ -1,9 +1,9 @@
 def hex_dump(data):
-    print '==================='
+    print('===================')
     for v in data:
         value = '0x%x' % v
-        print value,
-    print ''
+        print(value)
+    print( '' )
 
 target = '01234567'.upper()
 key = 'GuessKey'
@@ -22,12 +22,12 @@ hex_dump(enc)
 calc_key = xor( enc, map(ord, target) )
 
 target='##### FLAG{CTF_FINDPLANETEXT} ####'.upper()
-print target
+print(target)
 
 enc = xor( map(ord, key), map(ord, target) )
 hex_dump(enc)
 
 enc = xor(calc_key ,enc )
 for v in calc_key:
-    print chr(v)
+    print(chr(v))
 
